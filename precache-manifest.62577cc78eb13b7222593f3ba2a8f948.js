1143878639600d032c68f56faea1d3107e800b2e
Okolo Johnbosco <chukwuemekaokolo233@gmail.com>
2020-08-02 15:39:18 +0100

    Updates

---
+++ precache-manifest.62577cc78eb13b7222593f3ba2a8f948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2192cbdc7d97d599f1fb72cbb41edb9d",
+    "url": "/Covid19-App/index.html"
+  },
+  {
+    "revision": "ce1083105e820577a0fd",
+    "url": "/Covid19-App/static/css/main.ef7d1508.chunk.css"
+  },
+  {
+    "revision": "8f2dd16b7ddcbfff44cc",
+    "url": "/Covid19-App/static/js/2.a9aaa0fb.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-App/static/js/2.a9aaa0fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce1083105e820577a0fd",
+    "url": "/Covid19-App/static/js/main.5c6e4ad2.chunk.js"
+  },
+  {
+    "revision": "f795b015f08e8b448206",
+    "url": "/Covid19-App/static/js/runtime-main.2c7054cd.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid19-App/static/media/image.d7265326.png"
+  }
+]);
